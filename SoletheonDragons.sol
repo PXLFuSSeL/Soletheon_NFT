2b56c06e08d8fa0ab6e334ac3de0c433423fd6ae
PXLFuSSeL <sbegaa@gmail.com>
2024-01-07 16:21:32 +0100

    last preparement for development

---
+++ SoletheonDragons.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SoletheonDragons is ERC721 {
+    constructor() {
+        
+    }
+}
